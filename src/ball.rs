94711d37ca2283c16df4caf42ce9f86ea64af056
gallant <vincentknightwork@gmail.com>
2023-02-12 21:52:04 -0600

    the game ever

--- src/ball.rs
+++ src/ball.rs
@@ -13,13 +13,41 @@ impl Ball {
         let ybool = rand::random();
         let x_a: i32;
         let y_a: i32;
-        if xbool { x_a = 1;} else { x_a = -1;}
-        if ybool { y_a = 1;} else { y_a = -1;}
+        if xbool {
+            x_a = 1;
+        } else {
+            x_a = -1;
+        }
+        if ybool {
+            y_a = 1;
+        } else {
+            y_a = -1;
+        }
 
         Ball {
             sprite: Sprite::from_bytes(include_bytes!("../ping.png"), x, y).unwrap(),
             vel_x: x_a,
             vel_y: y_a,
         }
+    }
+
+    pub fn rand_angle(&mut self) {
+        let xbool = rand::random();
+        let ybool = rand::random();
+        let x_a: i32;
+        let y_a: i32;
+        if xbool {
+            x_a = 1;
+        } else {
+            x_a = -1;
+        }
+        if ybool {
+            y_a = 1;
+        } else {
+            y_a = -1;
+        }
+
+        self.vel_x = x_a;
+        self.vel_y = y_a;
     }
 }
